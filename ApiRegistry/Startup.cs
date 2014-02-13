0b9d83781f18f926b1b5430332dcb99ebfbb480c
ashic <ashic@live.com>
2014-02-13 00:18:31 +0000

    initial commit

---
+++ ApiRegistry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApiRegistry.Startup))]
+namespace ApiRegistry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
